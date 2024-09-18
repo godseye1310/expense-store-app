71ee5003dbfe0476be803b43de7f84dca40cc86c
Avinash <avinashkmg1310@gmail.com>
2024-09-18 19:04:03 +0530

    testing

---
+++ src/components/Layout/Header.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+test("renders Home as a text", () => {
+	//Arrangle
+	render(<Header />);
+	//Act
+	//..nothing
+	//Assert
+	const navHomeElement = screen.getByTestId("Home", {
+		exact: false,
+	});
+	expect(navHomeElement).toBeInTheDocument();
+});
